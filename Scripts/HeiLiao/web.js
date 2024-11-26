2026c777d5e8437fa828c10877efb522538eb005
yuheng <onz3vyyds@gmail.com>
2024-11-26 15:09:11 +0800

    feat: 去除详情页推广

--- Scripts/HeiLiao/web.js
+++ Scripts/HeiLiao/web.js
@@ -1,7 +1,7 @@
 let body = $response.body;
 
 if (/<\/head>/.test(body)) {
-  body = body.replace(/<\/head>/, "</head><style>.addbox,#notice_container,.gotoclick,.list-sec{display:none !important;}</style>");
+  body = body.replace(/<\/head>/, "</head><style>.editormd-preview blockquote,.footer-desc,.addbox,#notice_container,.gotoclick,.list-sec{display:none !important;}</style>");
   $done({ body });
 }
 
